a32e63e2073482fc0c2f34421bf17b1c57b9fac5
remain <15388635773@163.com>
2019-10-21 22:11:41 +0800

    first commit

---
+++ src/test/java/cn/test/springboot_jdbc/SpringbootJdbcApplicationTests.java
@@ -0,0 +1,38 @@
+package cn.test.springboot_jdbc;
+
+import cn.test.springboot_jdbc.entity.Student;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootJdbcApplicationTests {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+//        Connection connection = dataSource.getConnection();
+//        System.out.println("当前使用的连接池:"+connection.getClass());
+//        jdbcTemplate.update("insert into student values(null,?,?)","赵六",12);
+//        Long count = jdbcTemplate.queryForObject("select count(*) from student", Long.class);
+//        System.out.println(count);
+        List<Student> studentList = jdbcTemplate.query("select * from student", new BeanPropertyRowMapper<Student>(Student.class));
+        System.out.println(studentList);
+
+    }
+
+}
